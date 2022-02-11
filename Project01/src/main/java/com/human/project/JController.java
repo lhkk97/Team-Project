4c3311a9ab0d666da0aba3e35a360664d35c6fa2
honeyamy <honeyamy@DESKTOP-T608VN7>
2022-02-11 17:24:56 +0900

    220211 - joohee02

--- Project01/src/main/java/com/human/project/JController.java
+++ Project01/src/main/java/com/human/project/JController.java
@@ -1,8 +1,20 @@
 package com.human.project;
 
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class JController {
-
+	
+	@RequestMapping(value = "/book_done", method = RequestMethod.GET)
+	public String book_done() {
+		
+		return "book_done";
+	}
 }
---
+++ Project01/src/main/java/com/human/project/iJBook.java
@@ -0,0 +1,5 @@
+package com.human.project;
+
+public interface iJBook {
+
+}
