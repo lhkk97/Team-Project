b52c4d179de6a56b22fc3d9f03806208a3582cc6
lhkk97 <lhkk97@naver.com>
2022-02-16 09:14:32 +0900

    member 관리

--- Project01/src/main/java/com/human/project/HController.java
+++ Project01/src/main/java/com/human/project/HController.java
@@ -51,6 +51,29 @@ public class HController {
 		return ja.toString();
 	}
 	
+	// 회원 삭제
+	@ResponseBody
+	@RequestMapping(value = "/deleteMember", method = RequestMethod.POST)
+	public String Member(HttpServletRequest hsr) {
+		String check=hsr.getParameter("check");
+		String[] userid=check.split(",");
+		
+		String str="";
+		try {
+			iMember member=sqlSession.getMapper(iMember.class);
+			
+			for(int i=0;i<userid.length;i++) {
+				System.out.println("["+userid[i]+"]");
+				member.deleteMember(userid[i]);
+			}
+			str="ok";
+		} catch(Exception e) {
+			str="fail";
+		}
+		System.out.println(str);
+		return str;
+	}
+	
 	// 등급 수정 dialog -> select
 	@ResponseBody
 	@RequestMapping(value = "/dlgType", produces="application/json;charset=utf-8")
@@ -68,6 +91,25 @@ public class HController {
 		return ja.toString();
 	}
 	
+	// dialog input 
+	@ResponseBody
+	@RequestMapping(value = "/getMember",produces="application/json;charset=utf-8")
+	public String getMember(HttpServletRequest hsr) {		
+		iMember type=sqlSession.getMapper(iMember.class);
+		ArrayList<MemberType> alType=type.getMember();
+		
+		JSONArray ja=new JSONArray();
+		for(int i=0; i<alType.size(); i++) {
+			JSONObject jo=new JSONObject();
+			jo.put("userid",alType.get(i).getUserid());
+			jo.put("m_code",alType.get(i).getCode());
+			jo.put("m_type",alType.get(i).getType());
+			ja.add(jo);
+		}
+		return ja.toString();
+	}
+	
+	// update(등급 수정)
 	@ResponseBody
 	@RequestMapping(value="/updateType", method=RequestMethod.POST)
 	public String updatType(HttpServletRequest hsr) {
