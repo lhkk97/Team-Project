bb19ca7e425e4408d6581a52480c53e0ebab3bd3
User <User@DESKTOP-8BF1BS3>
2022-02-16 10:52:54 +0900

    0216

--- Project01/src/main/java/com/human/project/YController.java
+++ Project01/src/main/java/com/human/project/YController.java
@@ -3,12 +3,14 @@ package com.human.project;
 import java.util.ArrayList;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.human.project.Reservation;
@@ -60,18 +62,39 @@ public class YController {
 	@RequestMapping("/reservation_update")
 	public String getRezUp() {
 		
-		
 		return "";
 	}
 	
-//	@RequestMapping("/addRez")
-//	public String addRez(HttpServletRequest hsr) {
-//		String strCode = hsr.getParameter("book_id");
-//		String booker = hsr.getParameter("booker");
-//		int room_id = Integer.parseInt(hsr.getParameter("room_id"));
-//		
-//		iRez rez = sqlSession.getMapper(iRez.class);
-//		return "";
-//	}
+	@RequestMapping(value="/insertBook", method = RequestMethod.POST, produces = "application/json; charset=utf-8")
+	public String insertBook(HttpServletRequest hsr, HttpSession session, Model model) {
+		String roomid = hsr.getParameter("roomid");
+		String in_date = hsr.getParameter("in_date");
+		String out_date = hsr.getParameter("out_date");
+		String booker = hsr.getParameter("booker");
+		
+		iJBook ibook = sqlSession.getMapper(iJBook.class);
+		ibook.insertBook(booker, roomid, in_date, out_date);
+		
+// 	book_done 출력용
+//		String roomtype = hsr.getParameter("roomtype");
+//		String roomname = hsr.getParameter("roomname");
+//		String howmany = hsr.getParameter("howmany");
+//		String howmuch = hsr.getParameter("howmuch");
+
+//		model.addAttribute(roomtype, "roomtype");
+//		model.addAttribute(in_date, "in_date");
+//		model.addAttribute(out_date, "out_date");
+//		model.addAttribute(roomname, "roomname");
+//		model.addAttribute(roomid, "roomid");
+//		model.addAttribute(howmany, "howmany");
+//		model.addAttribute(booker, "booker");
+//		model.addAttribute(howmuch, "howmuch");
+		return "book_done";
+	}
 	
+	@RequestMapping("/book_done")
+	public String book_done() {
+		
+		return "book_done";
+	}
 }
